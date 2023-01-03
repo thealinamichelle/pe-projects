1079fc61d14867582e30f8ba9d90aff76c4a48aa
Alina Pritchett <alina.michelle@me.com>
2023-01-02 18:43:10 -0800

    add loops practice
    Added grid styling

--- alina/projects/monsters/index.php
+++ alina/projects/monsters/index.php
@@ -13,12 +13,13 @@
 </head>
 
 <body>
+
+	<main>
+	<header>
+		<h1>Monsters</h1>
+	</header>
 <inner-column>
-<h1>Monsters</h1>
-
-
-
-	<div class="adoption">
+	<div class="adoption card">
 
 		
 		<?php $array = ["one", 1, "three", "cat", true, false];
@@ -53,10 +54,42 @@
 ?>
 
 	</div>
-	</div>
+
+
+<div class="fruit card">
+
+<?php
+
+	$fruits = ["apple", "banana", "carrot", "diakon", "eggplant", "grape", "fruit"];
+
+	echo "<ol>";
+	//  for each thing inside of fruit do as this codeblock says
+	foreach ($fruits as $fruit){
+		echo "<li>" . $fruit . "</li>";
+	}
+
+	echo "</ol>";
+?>
+
+</div>
+
+<div class="testing-ground card">
+
+<?php
+
+	$civilizationSong = ["Don't want no bright lights, false teeth, doorbells, landlords, I make it clear.", "Don't want no penthouse, bathtub, streetcars, taxis, noise in my ear.", "Don't want no jailhouse, (shotgun) fish hooks (golf clubs) I got my spears."];
+
+	echo "<ul>";
+	foreach ($civilizationSong as $bingo){
+		echo "<div><p> So bongo, bongo, bongo, I don't want to leave the congo, oh no no no no no Bingo, bangle, bungle, I'm so happy in the jungle, I refuse to go." .  "<span style='font-weight:600;'>" . $bingo . "</span> (So, no matter how they coax him) I'll stay right here." . "</p></div>";
+	};
+
+	echo "</ul>"
+?>
+
 </div>
 </inner-column>
-
+</main>
 </body>
 </html>
 
