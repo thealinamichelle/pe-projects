69bf7b3969a9e0e7720a88d3a712469f9a67cd50
Alina Pritchett <alina.michelle@me.com>
2023-01-04 15:16:36 -0800

    add function lesson

---
+++ alina/projects/functions/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<?php include("head.php"); ?>
+<?php include("monster-factory.php"); ?>
+
+
+<body>
+
+	<main>
+
+		<!-- <inner-column> -->
+
+
+			<?php include("functions.php");?>
+
+	
+
+
+
+
+
+
+		<!-- </inner-column> -->
+	</main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ alina/projects/functions/monster-factory.php
@@ -0,0 +1,80 @@
+<?php 
+
+	// $overlays = ["#f5097d", "#f0769a", "#f36a65", "#be8adb", "#f1c547"];
+
+
+
+	$dreamlu = [
+		"id" => "4002",
+		"name" => "Dreamlu",
+		"favoritefood" => "wall boogers",
+		"age" => "4",
+		"adoption status" => true, 
+		"portrait" => "images/dreamlu.jpg",
+		"color" => "#f5097d",
+	];
+
+
+	$lima = [
+		"id" => "4003",
+		"name" => "Lima Bean",
+		"favoritefood" => "lima beans",
+		"age" => 9,
+		"adoption status" => false, 
+		"portrait" => "images/lima.jpg",
+		"color" => "#f0769a",
+	];
+
+	$reads = [
+		"id" => "4004",
+		"name" => "Miss Readsalot",
+		"favoritefood" => "the yummiest pages from Cook's Illustrated",
+		"age" => 5,
+		"adoption status" => false, 
+		"portrait" => "images/reads.jpg",
+		"color" => "#f36a65",
+	];
+
+	$fragoo = [
+		"id" => "4005",
+		"name" => "Fragoo",
+		"favoritefood" => "rocks",
+		"age" => 8,
+		"adoption status" => false, 
+		"portrait" => "images/fragoo.jpg",
+		"color" => "#be8adb",
+	];
+
+	$codey = [
+		"id" => "4006",
+		"name" => "Codey",
+		"favoritefood" => "Mountain Dew Code Red",
+		"age" => 7,
+		"adoption status" => false, 
+		"portrait" => "images/codey.jpg",
+		"color" => "#f1c547",
+	];
+
+	$banana = [
+		"id" => "4007",
+		"name" => "Mr. Banana",
+		"favoritefood" => "tostones",
+		"age" => 17,
+		"adoption status" => false,
+		"portrait" => "images/banana.jpg", 
+		"color" => "#f5097d",
+	];
+
+	$grape = [
+		"id" => "4007",
+		"name" => "Grape",
+		"favoritefood" => "Oysters",
+		"age" => 2,
+		"adoption status" => false,
+		"portrait" => "images/grape.jpg",
+		"color" => "#f0769a",
+	];
+	
+	$monsters = [$dreamlu, $lima, $reads, $fragoo, $codey, $banana, $grape];
+
+	?>
