48623bec1c6c594436ad6d12d088c6667cea2c5a
Alina Pritchett <alina.michelle@me.com>
2023-01-07 19:49:38 -0800

    separated footer into two components

--- alina/footer.php
+++ alina/footer.php
@@ -1,72 +1,9 @@
-<?php
-
-$menuitems = [
-	
-	[ 
-		"link-name" => "writing",
-		"url" => "'https://alinamichelle.substack.com/' target='substack'"
-	], 
-
-	[ 
-		"link-name" => "style&nbsp;guide",
-		"url" => "'style-guide.php'"
-	], 
-
-
-	[
-		"link-name" => "resume",
-		"url" => "'resume.php'"
-	], 
-
-
-
-	[
-		"link-name" => "chat",
-		"url" => "'mailto:alina.michelle@me.com'"
-	], 
-
-
-	[ 
-		"link-name" => "work",
-		"url" => "'projects.php'"
-	], 
-
-	[ 
-		"link-name" => "goals",
-		"url" => "'goals.php'"
-	], 
-
-
-	[ 
-		"link-name" => "home",
-		"url" => "'index.php'"
-	],
-
-];
-
-
-?>
-
-
-
-
-
-
-
-
+</inner-column>
 </main>
 <footer>
+<?php include("navigation.php") ?>
 
-	<?php 
-	foreach($menuitems as $menuitem) { ?>
-
-
-		<div class="footer-color-box"> 
-			<a href=<?= $menuitem["url"] ?> class="footer-links"><?= $menuitem["link-name"];?></a>
-		</div>
-
-
-	<?php } ?>
+</inner-column>
 </footer>
 </body>
 </html>
