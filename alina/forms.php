9274cf43fd38360cb86d9560ff8e6f3159d2b525
Alina Pritchett <alina.michelle@me.com>
2023-01-13 21:34:29 -0800

    Add 3/4/5 forms to the list

--- alina/forms.php
+++ alina/forms.php
@@ -5,7 +5,7 @@
 $sections = [//A
 		[ //B
 		"heading" => "Retirement Calculator",
-		"url" =>"/projects/php/forms/one/index.php",
+		"url" =>"projects/e4p/one/index.php",
 		"works" => [//C
 			"<picture><img src='https://peprojects.dev/images/square.jpg'></picture>",
 			"project description",			
@@ -13,7 +13,36 @@ $sections = [//A
 		], //B
 				[ //D
 		"heading" => "Area of a Rectangular Room",
-		"url" =>"/projects/php/practice/two/index.php",
+		"url" =>"projects/e4p/two/index.php",
+		"works" => [//E
+			"<picture><img src='https://peprojects.dev/images/square.jpg'></picture>",
+			"project description",			
+				]//E
+		],//D
+
+		[ //Dthree
+		"heading" => "Hello There",
+		"url" =>"projects/e4p/two/index.php",
+		"works" => [//E
+			"<picture><img src='https://peprojects.dev/images/square.jpg'></picture>",
+			"project description",			
+				]//E
+		],//D
+
+
+		[ //Dfour
+		"heading" => "Character Counting",
+		"url" =>"projects/e4p/two/index.php",
+		"works" => [//E
+			"<picture><img src='https://peprojects.dev/images/square.jpg'></picture>",
+			"project description",			
+				]//E
+		],//D
+
+
+		[ //Dfive
+		"heading" => "Printing Quotes",
+		"url" =>"projects/e4p/five/index.php",
 		"works" => [//E
 			"<picture><img src='https://peprojects.dev/images/square.jpg'></picture>",
 			"project description",			
