02e9279175770961485f8ab6b02971b83278985b
Alina Pritchett <alina.michelle@me.com>
2023-01-18 01:57:11 -0800

    add hiking

--- alina/projects/hiking/data.php
+++ alina/projects/hiking/data.php
@@ -1,143 +1,153 @@
 <?php 
 
+$trails = [
 
-$chandler = [
-	"name" => "Chandler Motorway",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "34.33018, -118.34887",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "001",
+		"name" => "Chandler Motorway",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "34.33018, -118.34887",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$ferndell = [
-	"name" => "Ferndell Loop",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "002",
+		"name" => "Ferndell Loop",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$fryman = [
-	"name" => "Fryman Canyon",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "003",
+		"name" => "Fryman Canyon",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$runyon = [
-	"name" => "Runyon Canyon",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "004",
+		"name" => "Runyon Canyon",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$astral = [
-	"name" => "Astral Drive",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "005",
+		"name" => "Astral Drive",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$canyonback = [
-	"name" => "Canyon Back Trail",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "006",
+		"name" => "Canyon Back Trail",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$eastVirgines = [
-	"name" => "East Las Virgines Canyon Trail",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "007",
+		"name" => "East Las Virgines Canyon Trail",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$westridge = [
-	"name" => "Westridge Trail",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "008",
+		"name" => "Westridge Trail",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
-];
-$sullivan = [
-	"name" => "Sullivan Trail",
-	"short-desc" => "This is a short description of this great trail. Dogs just love it!",
-	"image" => "https://peprojects.dev/images/landscape.jpg",
-	"location" => "",
-	"leash laws" => "",
-	"difficulty" => "",
-	"ocean view" => "",
-	"trail width" => "",
-	"common hazards" => [
-		"ticks" => "",
-		"foxtail" => "",
-		"snakes" => "",
+	[
+		"id" => "009",
+		"name" => "Sullivan Trail",
+		"short-desc" => "This is a short description of this great trail. Dogs just love it!",
+		"image" => "https://peprojects.dev/images/landscape.jpg",
+		"location" => "",
+		"leash laws" => "",
+		"difficulty" => "",
+		"ocean view" => "",
+		"trail width" => "",
+		"common hazards" => [
+			"ticks" => "",
+			"foxtail" => "",
+			"snakes" => "",
+		]
 	],
 ];
 
 
-	$trails = [$chandler, $ferndell, $fryman, $runyon, $astral, $canyonback, $eastVirgines, $westridge, $sullivan];
-
+	
 ?>
