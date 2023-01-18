da2c21159f5d5e311c2cd9f8c27bd470e1ec12ec
Alina Pritchett <alina.michelle@me.com>
2023-01-18 01:57:22 -0800

    add hiking

--- alina/projects/hiking/trails.php
+++ alina/projects/hiking/trails.php
@@ -1,29 +1,40 @@
-<?php 
+<?php include("data.php");?>
 
 
 
-include("data.php");
 
 
 
-?>
+
+
 <div class="grid">
-	<?php foreach ($trails as $trail){?>
-		<section class="card">
-			<div>
-					<h2><?=$trail["name"]?></h2>
-			</div>
-			<div>
-				<picture>
-					<img src="<?=$trail["image"]?>">
-				</picture>
+				<?php foreach ($trails as $trail) {?>
+	<section class="card">
 
-					<p><?=$trail["short-desc"]?></p>
-			</div>
+
+		<div>
+
+
+			<a href="?page=details&trail=<?=$trail['id']?>">	
+				<h2><?=$trail["name"]?></h2>
+		</div>
+
+		<div>
+
+			<picture>
+
+				<img src="<?=$trail['image']?>">
+			</picture>
+
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit.</p>
+
+
+			</a>
+		</div>
 
 	</section>
-	<?php };?>
-</div>
+	<?php }?>
 
+</div>
 
 
