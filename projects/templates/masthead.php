b1a5e602e3f0ab7886097f3b67a3a10ed075b626
Alina Pritchett <alina.michelle@me.com>
2023-01-23 19:32:37 -0800

    add masthead from codepen project

---
+++ projects/templates/masthead.php
@@ -0,0 +1,17 @@
+<header>
+<inner-column>
+	
+	<masthead>
+		<nav class="site-menu">
+			<a href="#">Logo</a>
+			<a href="#">Some Page</a>
+			<a href="#">Some Other Page</a>
+		</nav>
+		
+		<nav class="user-menu">
+			<a href="#">Sign In</a>
+		</nav>
+	</masthead>
+
+</inner-column>
+</header>
