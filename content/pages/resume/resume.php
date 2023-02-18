b9385abad7b0649fec4791255cfd9d7eeb2f095b
Alina Pritchett <alina.michelle@me.com>
2023-02-17 16:03:16 -0800

    Add corrected if and foreach statements for Job roles

--- content/pages/resume/resume.php
+++ content/pages/resume/resume.php
@@ -11,73 +11,87 @@
 
 
 ?>
+
 <section class="inner-column">
+
+<header>
+<h1 class="loud-voice"><?=$title?></h1>
+</header>
 
 <?php foreach($sections as $section) {?>
 
-<div class="resume-card">
-<?php	echo $section["heading"] . "<br>";
-?>
-</div>
+	<div class="employment">
+
+
+		<h2 class="card-title"><?=$section["heading"]?></h2>
+
+			<?php
+			 foreach($section["jobs"] as $job) {?>
+				<div class="card">
+					<h3><?=$job["company"]?></h3>
+						
+					<ol class="details">
+						<li>years: <?=$job["years"]?></li>
+						<li>
+							<?php 
+
+								if (is_array($job["role"]) ) {
+									echo "<ol>";
+									foreach($job["role"] as $role) {
+										echo "<li> role: " . $role . "</li>";
+									} 
+									echo "</ol>";
+								}
+								else {
+									echo "role: " . $job["role"];
+								}
+							?>
+						</li>
+						<li>accomplishments: <?=$job["accomplishments"]?></li>
+						<li>job: <?=$job["tools"]?></li>
+					</ol>
+				</div>
+			<?php }?>
+	</div>
+
+
+
+	<div class="education">
+
+
+		<h2 class="card-title"><?=$section["heading"]?></h2>
+
+			<?php foreach($section["experience"] as $exp) {?>
+				<div class="card">
+					<h3><?=$exp["institution"]?></h3>
+						
+					<ol class="resume-details">
+						<li>years: <?=$exp["years"]?></li>
+						<?php foreach($exp["accomplishments"] as $accomplish) {?><li>role: <?=$accomplish?></li><?php }?>
+						<li>accomplishments: <?=$exp["accomplishments"]?></li>
+					</ol>
+				</div>
+			<?php }?>
+
+	</div>
+
+
+	<div class="skills">
+
+		<h2 class="card-title"><?=$section["heading"]?></h2>
+
+			<?php foreach($section["abilities"] as $skill) {?>
+				<div class="card">
+					<ol class="resume-details">
+<li><?=$skill?></li><?php// }?>
+					</ol>
+				</div>
+			<?php }?>
+
+	</div>
+
 <?php }?>
 
-<div class="resume-card">
-
-<?php 
-foreach($jobs as $job) {
-	echo $job;
-}
-
-		?>
-
-
-
-
-
-
-<?php
-	// 	foreach($sections["jobs"] as $job) {
-	// 		echo $job["company"] . "<br>";
-		// 	echo $job["years"] . "<br>";
-		// 	foreach($job["role"] as $role) { echo $role . "<br>";}
-		// 	echo $job["accomplishments"] . "<br>";
-		// 	echo $job["tools"] . "<br>";
-?>
-<!-- </div>
-<div class="resume-card"> -->
-
-<?php
-	// echo $section["heading"] . "<br>";
-	// 	foreach($section["experience"] as $exp) {
-	// 		echo $exp["institution"] . "<br>";
-	// 		echo $exp["years"] . "<br>";
-	// 		foreach($exp["accomplishments"] as $accomplish) {
-	// 			echo $accomplish . "<br>";
-	// 			}
-	// 		echo $exp["accomplishments"] . "<br>";
-	// 	}
-?>
-<!-- </div>
-<div class="resume-card"> -->
-<?php
-// 			echo $section["heading"] . "<br>";
-// 				foreach($section["abilities"] as $skill) {
-// 						echo $skill . "<br>";
-// 			}
-// }
-
-
-
-
-
-
-
-
-
-
-?>
-
-</div>
 </section>
 
 <?php// formatcode($resumeData);
