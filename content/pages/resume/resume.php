cfd1b664ae4f4336abb7acf04c5be43bedfa5654
Alina Pritchett <alina.michelle@me.com>
2023-02-19 13:03:48 -0800

    Fixed looping erros

--- content/pages/resume/resume.php
+++ content/pages/resume/resume.php
@@ -6,10 +6,6 @@
 	$title = $resumeData["pageTitle"];
 	$intro =  $resumeData["introduction"];
 	$sections = $resumeData["sections"];
-	$jobs = $sections["jobs"]
-
-
-
 ?>
 
 <section class="inner-column">
@@ -20,74 +16,62 @@
 
 <?php foreach($sections as $section) {?>
 
-	<div class="employment">
-
+	<div class="<?=$section['id']?>">
 
 		<h2 class="card-title"><?=$section["heading"]?></h2>
 
 			<?php
-			 foreach($section["jobs"] as $job) {?>
-				<div class="card">
-					<h3><?=$job["company"]?></h3>
-						
-					<ol class="details">
-						<li>years: <?=$job["years"]?></li>
-						<li>
-							<?php 
+			if (isset($section["jobs"]) ){
+				 foreach($section["jobs"] as $job) {?>
+					<div class="card">
+						<h3><?=$job["company"]?></h3>
+							
+						<ol class="details">
+							<li>years: <?=$job["years"]?></li>
+							<li>
+								<?php 
 
-								if (is_array($job["role"]) ) {
-									echo "<ol>";
-									foreach($job["role"] as $role) {
-										echo "<li> role: " . $role . "</li>";
-									} 
-									echo "</ol>";
-								}
-								else {
-									echo "role: " . $job["role"];
-								}
-							?>
-						</li>
-						<li>accomplishments: <?=$job["accomplishments"]?></li>
-						<li>job: <?=$job["tools"]?></li>
-					</ol>
-				</div>
-			<?php }?>
-	</div>
-
-
-
-	<div class="education">
-
-
-		<h2 class="card-title"><?=$section["heading"]?></h2>
-
-			<?php foreach($section["experience"] as $exp) {?>
-				<div class="card">
-					<h3><?=$exp["institution"]?></h3>
-						
-					<ol class="resume-details">
-						<li>years: <?=$exp["years"]?></li>
-						<?php foreach($exp["accomplishments"] as $accomplish) {?><li>role: <?=$accomplish?></li><?php }?>
-						<li>accomplishments: <?=$exp["accomplishments"]?></li>
-					</ol>
-				</div>
+									if (is_array($job["role"]) ) {
+										echo "<ol>";
+										foreach($job["role"] as $role) {
+											echo "<li> role: " . $role . "</li>";
+										} 
+										echo "</ol>";
+									}
+									else {
+										echo "role: " . $job["role"];
+									}
+								?>
+							</li>
+							<?php foreach($job["accomplishments"] as $jobDesc) {?><li>role: <?=$jobDesc?></li><?php }?>
+							<li>job: <?=$job["tools"]?></li>
+						</ol>
+					</div>
+				<?php }?>
 			<?php }?>
 
-	</div>
-
-
-	<div class="skills">
-
-		<h2 class="card-title"><?=$section["heading"]?></h2>
-
-			<?php foreach($section["abilities"] as $skill) {?>
+			<?php if (isset($section["experience"]) ){?>
+				<?php foreach($section["experience"] as $exp) {?>
+					<div class="card">
+						<h3><?=$exp["institution"]?></h3>
+							
+						<ol class="resume-details">
+							<li>years: <?=$exp["years"]?></li>
+							<?php foreach($exp["accomplishments"] as $accomplish) {?><li>role: <?=$accomplish?></li><?php }?>
+						</ol>
+					</div>
+				<?php }?>
+			<?php }?>
+		 
+			<?php if (isset($section["abilities"]) ){?>
 				<div class="card">
 					<ol class="resume-details">
-<li><?=$skill?></li><?php// }?>
+					 	<?php foreach($section["abilities"] as $skill) {?>
+							<li><?=$skill?></li>
+						<?php }?>
 					</ol>
 				</div>
 			<?php }?>
-
 	</div>
 
 <?php }?>
