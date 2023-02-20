f4139eaec95efa2d9bbbaef845b7117001162990
Alina Pritchett <alina.michelle@me.com>
2023-02-19 21:25:56 -0800

    header- navigation if statements and yellow-nav removal

--- header.php
+++ header.php
@@ -6,13 +6,27 @@
 <?php
 /*ROUTER*/
 	$page = "home";
+
 	if ( isset($_GET["page"]) ) {
 		$page = $_GET["page"];
+		$footerNav = "";
+		$line = "border-bottom: var(--yellow) 1px solid; flex-flow: row-reverse wrap;";
 	}
 	else {
 		$page = "home";
+	}
+	if ($page == "home") {
+		$line = "border-top: var(--yellow) 1px solid; flex-flow: row wrap;";
 	}
 ?>
 <body>
 	
 	<main>
+<?php 
+if ($page !== "home") {
+	include ("content/modules/navigation/navigation.php");
+}
+
+?>
+
+	
