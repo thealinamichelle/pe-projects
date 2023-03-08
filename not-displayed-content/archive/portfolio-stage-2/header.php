92575cf18ded816813c635ff7ac94f4c2b26354e
Alina Pritchett <alina.michelle@me.com>
2023-03-07 18:11:28 -0800

    Portfolio site v2 archived

---
+++ not-displayed-content/archive/portfolio-stage-2/header.php
@@ -0,0 +1,9 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include("head.php")?>
+
+
+<body>
+	
+	<main>
---
+++ not-displayed-content/archive/portfolio-stage-2/index.php
@@ -0,0 +1,57 @@
+<?php include("header.php") ?>
+
+<nav-yellow-index>
+<section class="home-grid">
+			
+			<section class="visuals">
+				<?php include("alina-new-svg.php")?>
+
+					<div class="title-blue-square invisible-period"><p>.</p>
+					</div>
+
+				
+
+					<div class="dog">
+						<?php include("the-dog-svg.php")?>
+					</div>
+
+			</section>
+
+
+			<section class="info">
+				a dog walker and trainer turned web developer. 
+			</section>
+		
+			<section class="index-cards">
+				
+				<a href="https://peprojects.dev/alpha-6/alina/goals.php">goals</a>
+				
+				<a href="projects.php">work</a>
+				
+				<a href="mailto:alina.michelle@me.com">chat</a>
+				
+				<a href="resume.php">resume</a>
+
+				
+				<div class="yellow-1 invisible-period">
+					<p>.</p>
+				</div>
+				<div class="yellow-2 invisible-period">
+					<p>.</p>
+				</div>
+
+				<div class="yellow-3 jiggle invisible-period">
+					<p>.</p>
+				</div>
+
+				<div class="red-1 invisible-period">
+					<p>.</p>
+				</div>
+
+				<div class="green jiggle invisible-period">
+				</div>
+			</section>
+
+</section>
+</nav-yellow-index>
+<?php include("footer-index.php") ?>
