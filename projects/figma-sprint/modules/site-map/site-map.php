7e98f8f85452bee8d4f212ef37a5de2e63a93f46
Alina Pritchett <alina.michelle@me.com>
2023-04-03 21:04:07 -0700

    Design System integration with PHP

--- projects/figma-sprint/modules/site-map/site-map.php
+++ projects/figma-sprint/modules/site-map/site-map.php
@@ -6,63 +6,73 @@
 ?>
 <section class="footer site-map">
 
+
+
 <inner-column>
 	<grid>
 
 		<?php foreach($sections as $section){
-
-			$loc = $section["location"]; 
-			 if ($loc == "site-map") {
-
+			$heading = $section["heading"];
+			 
+			 if (isset($section["location"]) ){
+			 	$loc = $section["location"];
+			 	
+				 	if ($loc == "site-map"){
 			?>
-			<ul class="link-groups">
-				<h3><?=$section["heading"]?></h3>
+				<ul class="link-groups">
+					<h3><?=$heading?></h3>
 
-					<?php foreach($section["links"] as $link){?>
+						<?php foreach($section["links"] as $link){?>
 
-						<a href="<?=$link["url"]?>">
-							<li><?=$link["name"]?></li>
-						</a>
-					<?php }?>
-			</ul>
-		<?php	}
-			}
+							<a href="<?=$link["url"]?>">
+								<li><?=$link["name"]?></li>
+							</a>
+						<?php }?>
+				</ul>
+			<?php	}
+			};
+		}
+
+		if ($heading == "subscribe") {
+		}
 		?>
 
 		<div class="subscribe">
-			<h3>Subscribe</h3>
+			<h3><?=$heading?></h3>
 			<form method="POST">
-				<label for="email-address">
-				</label>
-				<input type="email" name="email">
-					<div class="small-button">
-						<svg width="15" height="8" viewBox="0 0 15 8" fill="none">
-							<path d="M13.5 7L7.5 1L1.5 7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-
-					</div>
+			<?php 
+			if (isset($section["form"]) ) { 
+					foreach ($section["form"] as $form) {?>
+						
+						<label for="<?=$form['for']?>"></label>
+						
+						<input type="<?=$form['input']?>" name="<?=$form['input']?>">
+					<?php mySubmitButton($form["class"]);
+					}
+			}
+			?>
 			</form>
 
-			<p class="fine-print">Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. </p>
-		</div>
+
+			<p class="fine-print"><?=$section["text"]?></div>
 	</grid>
 	<div>
 	<hr>
 </div>
 	<div class="footer-end">
 		
-		<picture class="logo">
+		<a href=""><picture class="logo">
 			<?php include("images/icons/logo.svg") ?>	
-		</picture>
+		</picture></a>
 
-		<a href="" class="fine-print">Terms</a>
-		<a href="" class="fine-print">Privacy</a>
-		<a href="" class="fine-print">Cookies</a>
+		<a href="#" class="fine-print">Terms</a>
+		<a href="#" class="fine-print">Privacy</a>
+		<a href="#" class="fine-print">Cookies</a>
 
 
-		<picture class="social-small"><img src="images/icons/in.svg"></picture>
-		<picture class="social-small"><img src="images/icons/f.svg"></picture>
-		<picture class="social-small"><img src="images/icons/wat.svg"></picture>
+		<a href="#"><picture class="social-small"><img src="images/icons/in.svg"></picture></a>
+		<a href="#"><picture class="social-small"><img src="images/icons/f.svg"></picture></a>
+		<a href="#"><picture class="social-small"><img src="images/icons/wat.svg"></picture></a>
 		
 </div>
 </inner-column>	
