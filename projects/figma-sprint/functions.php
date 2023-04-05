657b6e5c2735e1b772ba18f53ff319b3bd02e816
Alina Pritchett <alina.michelle@me.com>
2023-04-05 16:39:49 -0700

    added functions for forms

--- projects/figma-sprint/functions.php
+++ projects/figma-sprint/functions.php
@@ -34,6 +34,23 @@ function myRadioSetup($radioName){//A
 }//A;
 
 
+function myFormSetup($formValue) {
+	$formvalue = null;
+
+	if ( isset($_POST["$formvalue"])) {
+	 $formvalue = $_POST["$formvalue"];
+	}
+}
+
+
+function myIsChecked($radioName, $radioItem) {//A
+
+		if ($_POST["$radioName"] == $radioItem){//B if that the superglobal recieved the radio item and it matches the radio group name add "checked".
+			return "checked";
+		}//B
+	}//A
+
+
 function mySubmitButton($form){
 
 echo '<button class="';
@@ -52,23 +69,125 @@ echo '<svg width="15" height="8" viewBox="0 0 15 8" fill="none">
 
 
 
-function myFormSetup($formValue) {
-	$formvalue = null;
 
-	if ( isset($_POST["$formvalue"])) {
-	 $formvalue = $_POST["$formvalue"];
+
+function myButton ($array){
+	$customClass = "";
+	$target = "";
+
+	if (isset($array["class"]) ){
+			$customClass = $array["class"];
 	}
+
+	if (!str_contains($array["url"], "localhost:8888") ) {
+			$target = "target='$array[url]'";
+	}
+	if (!str_contains($array["url"], "#") ) {
+			$target = "target='$array[url]'";
+	}
+
+	$button = "<a href='$array[url]' class='$customClass button' $target>";
+	$button .= $array["text"];
+	$button .= "</a>";
+
+
+	return $button;
+}
+
+
+function myLink($array) {
+	$customClass = "";
+	$textClass = "";
+	$target = "";
+	$svg = null;
+	$text = null;
+	$url = "";
+
+	if (isset($array["class"]) ){
+			$customClass = $array["class"];
+	}
+	if (isset($array["text-class"]) ){
+			$textClass = "class = " . $array["text-class"];
+	}
+	if (isset($array["svg"]) ){
+			$svg = $array["svg"];
+	}
+
+	if (isset($array["text"]) ){
+			$text = "<p $textClass>$array[text]</p>";
+	}
+
+	if (!str_contains($array["url"], "localhost:8888") ) {
+			$target = "target='$array[url]'";
+	}
+	if (!str_contains($array["url"], "#") ) {
+			$target = "target='$array[url]'";
+	}
+
+	$link = "<div $customClass'><a href='$array[url]' $target>";
+	$link .= "<picture>";
+	$link .= print_svg($svg);
+	$link .= "</picture>";
+	$link .= $text;
+	$link .= "</a></div>";
+
+
+	return $link;
+}
+
+
+function myCheckandPrint($arrays, $array, $subarray, $function){
+
+		if (isset($arrays["$subarray"]) ) {
+				foreach ($arrays["$subarray"] as $array) {
+					echo $function($array);
+				}
+		}
+		if (!isset($arrays["$subarray"]) ) {
+			;
+		}
+	}
+
+function print_svg($file){
+    $iconfile = new DOMDocument();
+    $iconfile->load($file);
+    return $iconfile->saveHTML($iconfile->getElementsByTagName('svg')[0]);
 }
 
 
 
 
-function myIsChecked($radioName, $radioItem) {//A
 
-		if ($_POST["$radioName"] == $radioItem){//B if that the superglobal recieved the radio item and it matches the radio group name add "checked".
-			return "checked";
-		}//B
-	}//A
+
+function formButton ($array){
+	$customClass = "";
+
+	if (isset($array["class"]) ){
+			$customClass = $array["class"];
+	}
+
+	$button = "<button class='$customClass'>";
+	$button .= $array["text"];
+	$button .= "</button>";
+
+
+	return $button;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
